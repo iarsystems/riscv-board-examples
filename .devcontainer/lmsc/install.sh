abf23e20c268f32f13dfc97c0bcdc12f00cb9d78
David Kallberg <david.kallberg@iar.com>
2025-11-25 15:52:45 +0100

    bump lmsc to v1.9

--- .devcontainer/lmsc/install.sh
+++ .devcontainer/lmsc/install.sh
@@ -1,5 +1,5 @@
 #!/bin/env sh
 
-wget https://netstorage.iar.com/FileStore/STANDARD/001/003/926/iar-lmsc-tools_1.8_amd64.deb
+wget https://netstorage.iar.com/FileStore/STANDARD/001/003/984/iar-lmsc-tools_1.9_amd64.deb
 
-apt install -y ./iar-lmsc-tools_1.8_amd64.deb
+apt install -y ./iar-lmsc-tools_1.9_amd64.deb
